b8d304a3f248d1c5cedc34500ff0a5730ee308be
treiled <trefeels@gmail.com>
2025-03-24 15:04:18 -0400

    module 2 done need some fixing later

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
